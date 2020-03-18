3a65e31fd0de2b0ffed51098fd12217b764283b1
shawn <shawnsun93@gmail.com>
2020-03-18 14:46:13 +0800

    feat(palindrom): longes palindromic substr

---
+++ LeetCode/P5_longest_palindromic_substring.cc
@@ -0,0 +1,68 @@
+/**
+ * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+    Input: "babad"
+    Output: "bab"
+    Note: "aba" is also a valid answer.
+*/
+
+
+#include <iostream>
+#include <string>
+
+#include "utils/include/time_log.h"
+#include "utils/include/utils.h"
+
+using namespace std;
+
+class Solution {
+   public:
+    string longestPalindrome(string s) {
+        if (s.length() <= 0) {
+            return "";
+        }
+
+        int substr_start = 0;
+        int substr_end = 0;
+        int len = 0;
+        int len_odd = 0, len_even = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            // 以元素i为中心，技术情况下
+            len_odd = ExpandCenter(s, i, i);
+            // 以元素i和i+1之间为中心，偶数情况下
+            len_even = ExpandCenter(s, i, i + 1);
+            len = len_odd > len_even ? len_odd : len_even;
+
+            if (len > substr_end - substr_start) {
+                substr_start = i - (len - 1) / 2;
+                substr_end = i + len / 2;
+            }
+        }
+
+        // 这里不能直接用len，可能len会被后面小的len替换掉
+        return s.substr(substr_start, substr_end - substr_start + 1);
+    }
+
+   private:
+    int ExpandCenter(const std::string& s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s[L] == s[R]) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+};
+
+int main(int argc, char* argv[]) {
+    std::string str = "babad";
+
+    Solution sln;
+    {
+        TimeLog scope("longestPalindrome");
+        auto res = sln.longestPalindrome(str);
+        std::cout << "res = " << res << std::endl;
+    }
+
+    return 0;
+}
